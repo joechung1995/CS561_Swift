88ef50dc919fe728bee0510435d91d2f2423dd30
Joe <joechung1995@gmail.com>
2022-10-16 01:01:35 -0700

    Adding unit and integration tests

--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -4,9 +4,19 @@ public protocol WeatherService {
     func getTemperature() async throws -> Int
 }
 
-class WeatherServiceImpl: WeatherService {
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=<INSERT YOUR API KEY HERE>"
 
+enum BaseUrl: String {
+    case openweathermap = "https://api.openweathermap.org"
+    case mockServer = "127.0.0.1:8080"
+}
+
+let apiKey = "6810dc9f50f54b90289eb8e83ed6fd14"
+
+class WeatherServiceImpl: WeatherService {
+    let url = "\(BaseUrl.openweathermap.rawValue)/data/2.5/weather?q=corvallis&units=imperial&appid=\(apiKey)"
+    //let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=6810dc9f50f54b90289eb8e83ed6fd14"
+    //let url = "https://api.openweathermap.org/data/2.5/weather?q=corvallis&units=imperial&appid=<INSERT YOUR API KEY HERE>"
+    
     func getTemperature() async throws -> Int {
         return try await withCheckedThrowingContinuation { continuation in
             AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
@@ -22,9 +32,11 @@ class WeatherServiceImpl: WeatherService {
             }
         }
     }
+    
+    
 }
 
-private struct Weather: Decodable {
+struct Weather: Decodable {
     let main: Main
 
     struct Main: Decodable {
--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-import MyLibrary
+@testable import MyLibrary
 
 final class MyLibraryTests: XCTestCase {
     func testIsLuckyBecauseWeAlreadyHaveLuckyNumber() async {
@@ -67,6 +67,99 @@ final class MyLibraryTests: XCTestCase {
 
         // Then
         XCTAssertNil(isLuckyNumber)
+    }
+
+    // Q5. Unit Test
+    func testWeather() async {
+        //let realWeatherService = WeatherServiceImpl()
+        //let myLibrary = MyLibrary(weatherService: realWeatherService)
+        
+        // Given
+        let jsonString = """
+        {
+            "coord": {
+                "lon": -123.262,
+                "lat": 44.5646
+            },
+        
+            "weather": {
+                "id": 801,
+                "main": "Cloud",
+                "description": "few clouds",
+                "icon": "02d"
+            },
+        
+            "base": "stations",
+        
+            "main" : {
+                "temp": 17.24,
+                "feels_like": 16.9,
+                "temp_min": 16.95,
+                "temp_max": 22.83,
+                "pressure": 1019,
+                "humidity": 72
+            },
+        
+            "visibility": 10000,
+        
+            "wind": {
+                "speed": 5.66,
+                "deg": 40,
+                "gust": 8.23
+            },
+        
+            "clouds": {
+                "all": 20
+            },
+        
+            "dt": 1664573310,
+        
+            "sys": {
+                "type": 1,
+                "id": 3727,
+                "country": "US",
+                "sunrise": 1664546983,
+                "sunset": 1664589366
+            },
+        
+            "timezone": -25200,
+            "id": 5720727,
+            "name": "Corvallis",
+            "cod": 200
+        }
+        """
+        
+        // When
+        // Create json data from your json string
+        let jsonData = Data(jsonString.utf8)
+        
+        // Create a JSON decoder
+        let jsonDecoder = JSONDecoder()
+        
+        // Deserialize the data into a model
+        let weather = try! jsonDecoder.decode(Weather.self, from: jsonData)
+        
+        // Then
+        XCTAssert(weather.main.temp == 17.24)
+    }
+    
+    
+    // Q6. Integration Test
+    func testWeatherInegration() async {
+        // Given
+        let weatherService = WeatherServiceImpl()
+        var temp = 0
+        
+        // When
+        do {
+            try temp = await weatherService.getTemperature()
+            print(temp)
+        } catch {
+            print("Integration error")
+        }
+        
+        // Then
+        XCTAssertNotNil(temp)
     }
 
 }
